1123c90309cbc1b9ef4f6a66a1c0df48b512f755
Anupam Anand <thewarlord858@gmail.com>
2020-11-09 12:33:47 +0530

    Add files via upload

---
+++ src/components/Form.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Form = ({setInputText,inputText, todos, setTodos, setStatus}) => {
+
+     const submitTodoHandler = (e) =>{
+         e.preventDefault();
+        setTodos([...todos, {text: inputText, completed: false, id: Math.random() }]);
+
+        setInputText("");
+     }
+
+     const textChangedHandler = (e) =>{
+         setInputText(e.target.value);
+     }
+
+     const statusHandler= (e) =>{
+        setStatus(e.target.value);
+     }
+
+    return (<form>
+        <input onChange={textChangedHandler} type="text" value={inputText} className="todo-input" />
+        <button onClick={submitTodoHandler} className="todo-button" type="submit">
+          <i className="fas fa-plus-square"></i>
+        </button>
+        <div className="select">
+          <select onChange={statusHandler} name="todos" className="filter-todo">
+            <option value="all">All</option>
+            <option value="completed">Completed</option>
+            <option value="uncompleted">Uncompleted</option>
+          </select>
+        </div>
+      </form>)
+}
+
+export default Form;
